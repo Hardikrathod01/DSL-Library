e87a0ab2663216ce8b292f9dbbb1d4bb32a76e13
Hardikrathod01 <42797234+Hardikrathod01@users.noreply.github.com>
2019-10-22 02:14:39 +0200

    up

--- src/org/foo/masterseed.groovy
+++ src/org/foo/masterseed.groovy
@@ -19,7 +19,7 @@ String globalConfig = workspace + "/src/org/foo/baseConfig.cfg"
 File globalConfigFile = new File(globalConfig)
 File configFile = new File(jobsFolder)
 String globalConfigContent = globalConfigFile.getText()
-
+echo "print"
 
  
         // Object for storing parsed json response
